4d55407f17117b626cea3efa50658afa5c8b1104
Muhammed Ömer Tütüncü <muhammedomertt@hotmail.com>
2025-03-28 14:25:11 +0300

    Behaviour Tree & NPC part_1

--- Assets/Scripts/Masa.cs
+++ Assets/Scripts/Masa.cs
@@ -2,55 +2,7 @@ using UnityEngine;
 
 public class Masa : MonoBehaviour
 {
-    public bool isGambling;
+    public bool IsEmpty = true; // Masa baþlangýçta boþ olsun.
 
-    private Highlight highlight;
-    public Color gamblingColor = Color.green;
-    public Color normalColor = Color.red;
-
-    private bool canToggle = false;
-
-    private void Start()
-    {
-        highlight = GetComponent<Highlight>();
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            canToggle = true;
-        }
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            canToggle = false;
-        }
-    }
-
-    private void Update()
-    {
-        if (canToggle && Input.GetKeyDown(KeyCode.C))
-        {
-            isGambling = !isGambling;
-            ModeTable();
-        }
-    }
-
-    private void ModeTable()
-    {
-        if (isGambling)
-        {
-            highlight.color = gamblingColor;
-            highlight.ToggleHighlight(true);
-        }
-        else
-        {
-            highlight.color = normalColor;
-            highlight.ToggleHighlight(true);
-        }
-    }
+    
 }
