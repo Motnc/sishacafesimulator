313f0456b1d377befb1fd992b801ab125f61f331
Oğuzhan Çınar <oguzhancinar6@icloud.com>
2025-06-15 17:39:24 +0300

    Level'ın son hâli alındı ve Open to Close tamamlandı.

---
+++ Assets/Scripts-Ozexe/Open to Close/OpenSignInteraction.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class OpenSignController : MonoBehaviour
+{
+    public Camera playerCamera;
+    public float interactionRange = 3f;
+    public Material greenMaterial;
+    public Material redMaterial;
+
+    public GameObject openTextObject; // OPEN yazýsý objesi
+
+    private bool isOpen = false;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = new Ray(playerCamera.transform.position, playerCamera.transform.forward);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, interactionRange))
+            {
+                if (hit.collider.CompareTag("OpenSign"))
+                {
+                    ToggleOpenTextMaterial();
+                }
+            }
+        }
+    }
+
+    void ToggleOpenTextMaterial()
+    {
+        if (openTextObject == null) return;
+
+        var renderer = openTextObject.GetComponent<Renderer>();
+        if (renderer == null) return;
+
+        isOpen = !isOpen;
+        renderer.material = isOpen ? greenMaterial : redMaterial;
+    }
+}
